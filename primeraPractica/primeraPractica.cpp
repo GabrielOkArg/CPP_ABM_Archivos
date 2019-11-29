1df6b4596f7657b4e54a48bc7579792b37337bb0
GabrielOkArg <eduardo.silva@neoris.com>
2019-11-29 17:30:39 -0300

    borrado de codigo

--- primeraPractica/primeraPractica.cpp
+++ primeraPractica/primeraPractica.cpp
@@ -27,97 +27,22 @@ struct miEstudiante
 int main()
 {
 	miEstudiante persona;
-	/*ofstream fichero;
-	fichero.open("fichero.csv", ios::app);
-
-	for (int i = 0; i < 2; i++)
-	{
-		cout << "Ingrese id, nombre, apellido,edad y promedio por favor: \n";
-		cin >> persona.id;
-		cin.ignore();
-		cin.getline(persona.nombre, 20);
-		cin.getline(persona.apellido, 20);
-		cin >> persona.edad;
-		cin >> persona.promedio;
-
-		fichero << persona.id << ";" << persona.nombre << ";" << persona.apellido << ";" << persona.edad << ";" << persona.promedio << "\n";
-		system("cls");
-	}
-	fichero.close();*/
-	/*ifstream lectura;
-	lectura.open("Cuentas.csv", ios::in);
-	string leido;
-	size_t found;
-	vector<Persona> Vpersona;
-	int contador = 0;
-	string x;
-	string y;
-	string Nombre_ = "";
-	int ID_ = 0;
-	while (!lectura.eof())
-	{
-		lectura >> leido;
-		try
-		{
-			found = leido.find(";");
-			{
-				contador = contador + 1;
-				Persona _cliente;
-				x = leido.substr(0, found);
-				y = leido.substr(found + 1, leido.size());
-
-				if (contador == 1)
-				{
-					ID_ = stoi(x);
-					Nombre_ = y;
-				}
-				if (contador == 2)
-				{
-					string aux = "";
-					_cliente.SetApellido(x);
-					for (int i = 0; i < y.length(); i++)
-					{
-						if (y[i] != '$')
-						{
-							aux += y[i];
-						}
-					}
-					_cliente.SetName(Nombre_);
-					_cliente.Setid(ID_);
-					_cliente.Setmonto(stoi(aux));
-					Vpersona.push_back(_cliente);
-					contador = 0;
-					ID_ = 0;
-					Nombre_ = "";
-				}
-
-
-			}
-		}
-		catch (const std::exception&)
-		{
-
-		}
-		
-	}*/
 	vector<Persona> Varray;
 	{
 		GestorDatos gestor;
-		vector<Persona> Varray = gestor.getList();
-		cout << "ID" << "   " << "Nombre" << "   " << "Apellido" << "   " << "Monto" << endl;
-		for (int i = 0; i < gestor.getList().size(); i++)
+		
+		for (int i = 0; i <gestor.getList().size(); i++)
 		{
-			cout << gestor.getList()[i].GetId() << "    " << gestor.getList()[i].GetName() << "   " << gestor.getList()[i].GetApellido() << " " << " $" << gestor.getList()[i].Getmonto() << endl;
+			Varray.push_back(gestor.getList()[i]);
+		}
+		cout << "ID" << "   " << "Nombre" << "   " << "Apellido" << "   " << "Monto" << endl;
+		for (int i = 0; i < Varray.size(); i++)
+		{
+			cout << Varray[i].GetId() << "    " << Varray[i].GetName() << "   " << Varray[i].GetApellido() << " " << " $" << Varray[i].Getmonto() << endl;
 		}
 	}
 	{
 		GestorDatos gestor;
-		/*Persona NewPeople;
-		NewPeople.Setid(55);
-		NewPeople.SetApellido("Fuentes");
-		NewPeople.SetName("Maru");
-		NewPeople.Setmonto(65500);
-		bool OK_ = gestor.AddNewItem(NewPeople);*/
 		cout << "ID" << "   " << "Nombre" << "   " << "Apellido" << "   " << "Monto" << endl;
 		for (int i = 0; i < gestor.getList().size(); i++)
 		{
